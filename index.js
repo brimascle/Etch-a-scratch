1f6a5f471ccc6ed0b53ce53a4e025e9e06c022d3
brimascle <hychung1110@gmail.com>
2023-08-29 16:16:40 -0400

    working with buttons

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
+let color = "black";
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    createBoard(32);
+    createBoard(16);
+
+    let btnPopup = document.querySelector('#popup');
+    btnPopup.addEventListener('click', () => {
+        let size = getSize();
+        createBoard(size);
+    });
 })
+
 
 function createBoard(size) {
     let board = document.querySelector('.board');
     
-    board.style.gridTemplateColumns = `repreat(${size}, 1fr)`;
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     let numberDivs = size * size;
 
     for (let i=0 ; i<numberDivs ; i++) {
         let div = document.createElement('div');
-        div.style.backgroundColor = 'yellow';
+        div.addEventListener('mouseover', colorDiv)
+
         board.insertAdjacentElement('beforeend', div);
     }
+}
+
+
+function colorDiv() {
+    if (color == "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = 'black';
+    }
+}
+
+
+function getSize() {
+    let input = prompt("What will be the size of the board?");
+    let message = document.querySelector('#message');
+    if (input > 0 && input <= 100) {
+        message.textContent = "Now you can play.";
+    } else if(input<0 || input>100) {
+        message.textContent = "Please put number between 1 to 100.";
+    } else {
+        message.textContent = "Please provide a number between 1 to 100";
+    } return input;
+}
+
+
+function setColor(colorChoice) {
+    color = colorChoice;
+}
+
+
+function resetBoard() {
+    let divs = document.querySelectorAll('div');
+    divs.forEach((div) => div.style.backgroundColor = 'white');
 }
